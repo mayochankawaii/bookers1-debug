50e25117e376755b0ef01903e85b9fb3287e9a8c
mayochankawaii <171436876+mayochankawaii@users.noreply.github.com>
2024-07-01 07:41:59 +0000

    bookers1-debug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,9 @@
 class BooksController < ApplicationController
+  
+  def new
+    @book = Book.new
+  end
+  
   def top
   end
 
@@ -13,6 +18,7 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
